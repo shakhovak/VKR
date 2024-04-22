9b16e2389750ac6d08bd6ea8743ed64bab948c95
shakhovak <89096305+shakhovak@users.noreply.github.com>
2024-04-22 21:27:32 +0300

    llama added

---
+++ ABSA_2nd_stage/main_llama.py
@@ -0,0 +1,199 @@
+import logging
+import argparse
+import os
+import warnings
+from datasets import load_dataset, DatasetDict
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    Trainer,
+    TrainingArguments,
+    DataCollatorForSeq2Seq,
+)
+from datasets import concatenate_datasets
+from huggingface_hub import HfFolder
+import torch
+from utils import (
+    preprocess_llama,
+    generate_and_tokenize_prompt,
+    Prompter,
+    evaluate_llama,
+)
+import wandb
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    prepare_model_for_int8_training,
+)
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("dataset_name", help="name of dataset")
+parser.add_argument("dataset_split", help="split of dataset")
+parser.add_argument("model_id_tokenizer", help="model id from hugging face")
+parser.add_argument("model_id_model", help="model id from hugging face")
+parser.add_argument("experiment_name", help="name of experience")
+parser.add_argument("-i", "--input", required=True, help="Input file")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+    logger = logging.getLogger()
+
+    # read files
+    logger.info("Reading and augementing datasets...")
+    raw_datasets = load_dataset(args.dataset_name, args.dataset_split)
+    raw_datasets = concatenate_datasets(
+        [raw_datasets["train"], raw_datasets["test"], raw_datasets["trial"]],
+    )
+    raw_datasets = raw_datasets.shuffle()
+    if args.dataset_split == "restaurants":
+        raw_datasets = raw_datasets.remove_columns(["aspectCategories"])
+
+    dataset_train_test = raw_datasets.train_test_split(test_size=0.1)
+    dataset_test_valid = dataset_train_test["test"].train_test_split(test_size=0.5)
+    final_ds = DatasetDict(
+        {
+            "train": dataset_train_test["train"],
+            "test": dataset_test_valid["train"],
+            "val": dataset_test_valid["test"],
+        }
+    )
+    instruction_dataset = final_ds.map(
+        preprocess_llama, fn_kwargs={"prompts_file_path": args.input}
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id_tokenizer)
+    tokenizer.pad_token_id = 0
+
+    prompter = Prompter()
+
+    train_data = (
+        instruction_dataset["train"]
+        .shuffle()
+        .map(
+            generate_and_tokenize_prompt,
+            fn_kwargs={
+                "prompter": prompter,
+                "tokenizer": tokenizer,
+            },
+        )
+    )
+    test_data = (
+        instruction_dataset["test"]
+        .shuffle()
+        .map(
+            generate_and_tokenize_prompt,
+            fn_kwargs={
+                "prompter": prompter,
+                "tokenizer": tokenizer,
+            },
+        )
+    )
+    val_data = (
+        instruction_dataset["val"]
+        .shuffle()
+        .map(
+            generate_and_tokenize_prompt,
+            fn_kwargs={
+                "prompter": prompter,
+                "tokenizer": tokenizer,
+            },
+        )
+    )
+
+    logger.info("Loading model...")
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    # load model from the hub
+    model_base = AutoModelForCausalLM.from_pretrained(
+        args.model_id_model, load_in_8bit=True, torch_dtype=torch.float16
+    )
+    # Define LoRA Config
+    lora_config = LoraConfig(
+        r=64,
+        lora_alpha=64,
+        target_modules=[
+            "q_proj",
+            "v_proj",
+            "k_proj",
+            "o_proj",
+        ],
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    # prepare int-8 model for training
+    model = prepare_model_for_int8_training(model_base)
+
+    # add LoRA adaptor
+    model = get_peft_model(model, lora_config)
+
+    HfFolder.save_token(os.environ["hugging_face_login"])
+
+    os.environ["WANDB_PROJECT"] = "absa_fin"
+    os.environ["WANDB_LOG_MODEL"] = "false"
+    wandb.login(key=os.environ["wandb_login"])
+
+    # Hugging Face repository id
+    repository_id = f"{args.model_id_tokenizer.split('/')[1]}-absa-{args.dataset_split}"
+
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    )
+
+    # Define training args
+    training_args = TrainingArguments(
+        output_dir=repository_id,
+        per_device_train_batch_size=8,
+        gradient_accumulation_steps=4,
+        warmup_steps=2,
+        num_train_epochs=1,
+        max_steps=400,
+        learning_rate=3e-4,
+        fp16=True,
+        optim="adamw_torch",
+        # logging & evaluation strategies
+        evaluation_strategy="steps",
+        eval_steps=40,
+        report_to="wandb",
+        logging_steps=40,
+        save_strategy="no",
+        save_total_limit=1,
+        push_to_hub=False,
+        hub_model_id=repository_id,
+        hub_token=HfFolder.get_token(),
+        run_name=args.experiment_name,
+    )
+    # Create Trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_data,
+        eval_dataset=test_data,
+    )
+    model.config.use_cache = False
+    logger.info("Starting training...")
+    trainer.train()
+
+    wandb.finish()
+    # =============================================================================
+    # Evaluation
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.config.use_cache = True
+    model.eval()
+
+    logger.info("Starting evaluation...")
+    evaluate_llama(val_data, prompter, tokenizer, device, model, args.experiment_name)
+
+    # Save our tokenizer and create model card
+    logger.info("Starting loading model to hub...")
+    tokenizer.save_pretrained(repository_id)
+    trainer.create_model_card()
+    # Push the results to the hub
+    trainer.push_to_hub()
+    
+    logger.info("All done.")
--- ABSA_2nd_stage/utils.py
+++ ABSA_2nd_stage/utils.py
@@ -3,6 +3,10 @@ import json
 import wandb
 import pandas as pd
 import logging
+from typing import Union
+import torch
+from transformers import GenerationConfig
+import re
 
 
 def generate_response(
@@ -12,7 +16,7 @@ def generate_response(
     with open(prompts_path) as fp:
         template = json.load(fp)
     num = random.randint(0, len(template) - 1)
-    if task == 'one_c':
+    if task == "one_c":
         instruction = template[str(num)]
         input = f"{instruction}\n{question}\n"
     else:
@@ -245,4 +249,326 @@ def createABSA_dataset(sample, prompts_file_path):
     sample["aspect_polarities_input"] = (
         f"Task name: Aspect Term Extraction and Polarity Classification \n{instruction}\n{sample['text']}\n"
     )
+    return sample
+
+
+# ===============================================================================
+
+
+def preprocess_llama(sample, prompts_file_path):
+    with open(prompts_file_path) as fp:
+        template = json.load(fp)
+
+    num = random.randint(0, len(template) - 1)
+    instruction = template[str(num)]
+
+    sample["instruction"] = f"""{instruction} {sample['text']} """
+
+    sample["answer"] = ", ".join(
+        [f"{item['term']}:{item['polarity']}" for item in sample["aspectTerms"]]
+    )
+    sample["output"] = (
+        f"{sample['instruction']} Aspects and their polarity: {sample['answer']}"
+    )
+    sample["input"] = sample["text"]
+
+    return sample
+
+
+# ===============================================================================
+
+
+def tokenize(prompt, tokenizer, cutoff_len=256, add_eos_token=True):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=cutoff_len,
+        padding=False,
+        return_tensors=None,
+    )
+    if (
+        result["input_ids"][-1] != tokenizer.eos_token_id
+        and len(result["input_ids"]) < cutoff_len
+        and add_eos_token
+    ):
+        result["input_ids"].append(tokenizer.eos_token_id)
+        result["attention_mask"].append(1)
+
+    result["labels"] = result["input_ids"].copy()
+
+    return result
+
+
+# ===============================================================================
+def generate_and_tokenize_prompt(data_point, prompter, tokenizer):
+    full_prompt = prompter.generate_prompt(
+        data_point["instruction"],
+        data_point["output"],
+    )
+    tokenized_full_prompt = tokenize(full_prompt, tokenizer)
+    user_prompt = prompter.generate_prompt(data_point["instruction"])
+    tokenized_user_prompt = tokenize(user_prompt,
+                                     tokenizer,
+                                     add_eos_token=False)
+    user_prompt_len = len(tokenized_user_prompt["input_ids"])
+
+    tokenized_full_prompt["labels"] = [-100] * user_prompt_len + tokenized_full_prompt[
+        "labels"
+    ][
+        user_prompt_len:
+    ]  # could be sped up, probably
+    return tokenized_full_prompt
+
+
+# ===============================================================================
+
+
+class Prompter(object):
+
+    def generate_prompt(
+        self,
+        instruction: str,
+        label: Union[None, str] = None,
+    ) -> str:
+
+        res = f"{instruction}\nAnswer: "
+
+        if label:
+            res = f"{res}{label}"
+
+        return res
+
+    def get_response(self, output: str) -> str:
+        return output.split("Answer:")[1].strip()
+
+
+# ===============================================================================
+
+
+def generate_llama(
+    text,
+    prompter,
+    tokenizer,
+    device,
+    model,
+    temperature=0.1,
+    top_p=0.75,
+    top_k=40,
+    num_beams=2,
+    max_new_tokens=256,
+    **kwargs,
+):
+    prompt = prompter.generate_prompt(text)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    input_ids = inputs["input_ids"].to(device)
+    generation_config = GenerationConfig(
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        num_beams=num_beams,
+        **kwargs,
+    )
+    with torch.no_grad():
+        generation_output = model.generate(
+            input_ids=input_ids,
+            generation_config=generation_config,
+            return_dict_in_generate=True,
+            output_scores=True,
+            max_new_tokens=max_new_tokens,
+        )
+    s = generation_output.sequences[0]
+    output = tokenizer.decode(s, skip_special_tokens=True).strip()
+
+    return prompter.get_response(output)
+
+
+# ===============================================================================
+def evaluate_llama(test_dataset, prompter, tokenizer, device, model, exp_name):
+    TP_aspect = 0
+    FN_aspect = 0
+    FP_aspect = 0
+    TP_sent = 0
+    FN_sent = 0
+    FP_sent = 0
+    answers = pd.DataFrame()
+
+    run = wandb.init(
+        project="absa_fin",
+        name=exp_name,
+    )
+
+    wandb.define_metric("F1_A", summary="mean")
+    wandb.define_metric("F1_S", summary="mean")
+    wandb.define_metric("F1_macro_ABSA", summary="mean")
+    wandb.define_metric("F1_micro_ABSA", summary="mean")
+
+    logging.info(f"Evaluation for exp {exp_name} started...")
+
+    for item in test_dataset:
+        answer = generate_llama(
+            item["instruction"],
+            prompter=prompter,
+            tokenizer=tokenizer,
+            device=device,
+            model=model,
+        )
+        if "Aspects and their polarity:" in answer:
+            answer = answer.split("Aspects and their polarity:")[1].strip()
+            answer = re.sub("[^A-Za-z:, ]+", "", answer).strip()
+        else:
+            answer = answer.strip()
+            answer = re.sub("[^a-zA-Z:, ]+", "", answer).strip()
+
+        prediction = answer.lower()
+        prediction = prediction.replace('aspect name:', "")
+        prediction = (
+            prediction.replace("aspects and their polarity:", "")
+            .replace(".", "")
+            .strip()
+            .lower()
+        )
+        label = item["answer"].lower()
+        label = re.sub("[^a-zA-Z:, ]+", "", label).strip()
+        new_row = {"y_pred": prediction, "y_true": label}
+        answers = pd.concat([answers, pd.DataFrame([new_row])])
+
+        y_pred = prediction.split(",")
+        y_true = label.split(",")
+
+        aspects_true_lst = [item.split(":")[0] for item in y_true]
+        aspects_pred_lst = [item.split(":")[0] for item in y_pred]
+
+        for aspect in aspects_true_lst:
+            if aspect in aspects_pred_lst:
+                TP_aspect += 1
+            else:
+                FN_aspect += 1
+        for aspect in aspects_pred_lst:
+            if aspect not in aspects_true_lst:
+                FP_aspect += 1
+                FP_sent += 1
+
+        for item in y_true:
+            if item in y_pred:
+                TP_sent += 1
+            else:
+                FN_sent += 1
+    logging.info(f"Logging artefacts for experiement {exp_name} started...")
+    data_to_log = wandb.Table(dataframe=answers)
+    run.log({f"{exp_name}_preds": data_to_log})
+
+    F1_aspect = 2 * TP_aspect / (2 * TP_aspect + FN_aspect + FP_aspect)
+    F1_sent = 2 * TP_sent / (2 * TP_sent + FN_sent + FP_sent)
+    F1_macro = (F1_aspect + F1_sent) / 2
+    F1_micro = (
+        2
+        * (TP_aspect + TP_sent)
+        / ((2 * (TP_aspect + TP_sent)) + (FN_aspect + FN_sent + FP_aspect + FP_sent))
+    )
+
+    log_dict = {
+        "F1_A": F1_aspect,
+        "F1_S": F1_sent,
+        "F1_macro_ABSA": F1_macro,
+        "F1_micro_ABSA": F1_micro,
+    }
+    wandb.log(log_dict)
+    wandb.finish()
+    logging.info("All logging is done.")
+
+# ===============================================================================
+
+
+def preprocess_llama_fmt(sample, prompts_file_path, dataset_task):
+    with open(prompts_file_path) as fp:
+        template = json.load(fp)
+
+    num = random.randint(1, len(template) - 1)
+    if dataset_task == "ATE":
+        instruction = f"Task name: Aspect Term Extraction. {template[dataset_task][str(num)]}"
+        sample["instruction"] = f"""{instruction}. Text: {sample['text']}"""
+        sample["answer"] = ", ".join(
+            [item["term"] for item in sample["aspectTerms"]]
+        )
+        sample["output"] = (
+            f"{sample['instruction']} Aspects: {sample['answer']}"
+        )
+    elif dataset_task == "ACS":
+        instruction = f"Task name: Sentiment Polarity Classification. {template[dataset_task][str(num)]}"
+        sample["aspect_list"] = ", ".join([item["term"] for item in sample["aspectTerms"]])
+        sample["answer"] = ",".join(
+            [item["polarity"] for item in sample["aspectTerms"]]
+        )
+        sample["instruction"] = f"{instruction}. Sentence: {sample['text']} Aspects mentioned: {sample['aspect_list']}."
+        sample["output"] = f"{sample['instruction']} Polarities of the aspects mentioned: {sample['answer']}."
+    else:
+        instruction = f"Task name: Aspect Term Extraction and Polarity Classification. {template[dataset_task][str(num)]}"
+        sample["instruction"] = f"""{instruction} {sample['text']} """
+        sample["answer"] = ", ".join(
+            [f"{item['term']}:{item['polarity']}" for item in sample["aspectTerms"]]
+        )
+        sample["output"] = (
+            f"{sample['instruction']} Aspects and their polarity: {sample['answer']}"
+        )
+    sample["input"] = sample["text"]
+
+    return sample
+# ===============================================================================
+
+
+def preprocess_mistral(sample, prompts_file_path):
+    with open(prompts_file_path) as fp:
+        template = json.load(fp)
+
+    num = random.randint(0, len(template) - 1)
+    instruction = template[str(num)]
+
+    sample["instruction"] = f"""<s>[INST] {instruction} {sample['text']} [/INST]"""
+
+    sample["answer"] = ", ".join(
+        [f"{item['term']}:{item['polarity']}" for item in sample["aspectTerms"]]
+    )
+    sample["output"] = (
+        f"{sample['instruction']} Aspects and their polarity: {sample['answer']} </s>"
+    )
+    sample["input"] = sample["text"]
+
+    return sample
+# ===============================================================================
+
+
+def preprocess_mistral_fmt(sample, prompts_file_path, dataset_task):
+    with open(prompts_file_path) as fp:
+        template = json.load(fp)
+
+    num = random.randint(1, len(template) - 1)
+    if dataset_task == "ATE":
+        instruction = f"Task name: Aspect Term Extraction. {template[dataset_task][str(num)]}"
+        sample["instruction"] = f"""[INST] {instruction}. Text: {sample['text']} [/INST]"""
+        sample["answer"] = ", ".join(
+            [item["term"] for item in sample["aspectTerms"]]
+        )
+        sample["output"] = (
+            f"{sample['instruction']} Aspects: {sample['answer']}"
+        )
+    elif dataset_task == "ASC":
+        instruction = f"Task name: Sentiment Polarity Classification. {template[dataset_task][str(num)]}"
+
+        sample["aspect_list"] = ", ".join([item["term"] for item in sample["aspectTerms"]])
+        sample["answer"] = ",".join(
+            [item["polarity"] for item in sample["aspectTerms"]]
+        )
+        sample["instruction"] = f"[INST] {instruction}. Sentence: {sample['text']} Aspects mentioned: {sample['aspect_list']}. [/INST]"
+        sample["output"] = f"{sample['instruction']} Polarities of the aspects mentioned: {sample['answer']}"                    
+    else:
+        instruction = f"Task name: Aspect Term Extraction and Polarity Classification. {template[dataset_task][str(num)]}"
+        sample["instruction"] = f"""[INST] {instruction}. Text: {sample['text']} [/INST]"""
+        sample["answer"] = ", ".join(
+            [f"{item['term']}:{item['polarity']}" for item in sample["aspectTerms"]]
+        )
+        sample["output"] = (
+            f"{sample['instruction']} Aspects and their polarity: {sample['answer']}"
+        )
+    sample["input"] = sample["text"]
+
     return sample
